af165c43b76452a9b6e2e40f0b179200e4a234be
김성은 <kse012050@naver.com>
2024-10-16 05:12:08 +0900

    react router 최신 문법 공부, 페이지별 css 다르게 적용하는 법 공부해야한다

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from "../css/test.module.css";
+
+export default function Home() {
+    return (
+        <div className={styles.test}>
+            home
+        </div>
+    );
+}
+
