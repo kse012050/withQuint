ccf7aec2b3e49a1638e652458fde886640f9b131
김성은 <kse012050@naver.com>
2025-05-19 11:28:23 +0900

    회원가입 문자 인증 완료

--- src/components/MobileAuthentication.jsx
+++ src/components/MobileAuthentication.jsx
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { inputChange } from '../api/validation';
 import { postApi } from '../api/api';
 import Popup from './Popup';
 
-export default function MobileAuthentication(inputs, setInputs) {
+export default function MobileAuthentication({ inputs, setInputs }) {
     const [phoneAuth, setPhoneAuth] = useState()
     const [isPhoneNum, setIsPhoneNum] = useState(false)
     const [timeLeft, setTimeLeft] = useState(0);
     const timerRef = useRef(null);
+    const mobileRef = useRef(null);
+    const authRef = useRef(null);
     const [popup, setPopup] = useState();
 
     const onAuthSend = () => {
@@ -19,14 +21,21 @@ export default function MobileAuthentication(inputs, setInputs) {
                         description: message,
                         func: () => {
                             setIsPhoneNum(true)
-                            setTimeLeft(10)
                         }
                     })
                 }
             })
     }
 
-      const startTimer = () => {
+    useEffect(()=> {
+        if(isPhoneNum){
+            authRef.current.focus()
+            setTimeLeft(180)
+            startTimer()
+        }
+    }, [isPhoneNum])
+
+    const startTimer = () => {
         if (timerRef.current) return; // 이미 실행 중이면 중복 방지
 
         timerRef.current = setInterval(() => {
@@ -34,14 +43,21 @@ export default function MobileAuthentication(inputs, setInputs) {
             if (prev <= 1) {
                 clearInterval(timerRef.current);
                 timerRef.current = null;
-                return 0;
+                setIsPhoneNum(false)
+                mobileRef.current.focus()
+                return false;
             }
             return prev - 1;
         });
         }, 1000);
     };
 
-    // 시간 형식 (mm:ss)
+    const stopTimer = () => {
+        clearInterval(timerRef.current);
+        timerRef.current = null;
+        setIsPhoneNum(false)
+    }
+
     const formatTime = (seconds) => {
         const min = String(Math.floor(seconds / 60)).padStart(2, '0');
         const sec = String(seconds % 60).padStart(2, '0');
@@ -51,12 +67,17 @@ export default function MobileAuthentication(inputs, setInputs) {
     const onAuthCheck = () => {
         postApi('signUp/mobileAuthCheck', {...phoneAuth})
             .then(({ result, state, message } = {})=>{
-                if(result && state){
+                if(result){
                     setPopup({
                         title: '안내',
                         description: message,
                         func: () => {
-                            setIsPhoneNum(true)
+                            if(state){
+                                stopTimer()
+                                setInputs((prev)=>({...prev, mobile: phoneAuth.mobile}))
+                            }else{
+                                authRef.current.focus()
+                            }
                         }
                     })
                 }
@@ -73,13 +94,15 @@ export default function MobileAuthentication(inputs, setInputs) {
                     data-formet='numb'
                     data-validation='mobile'
                     maxLength='11'
+                    ref={mobileRef}
                     onChange={(e)=>{
-                        setIsPhoneNum(false)
+                        timerRef.current && stopTimer()
+                        inputs?.mobile && setInputs((prev)=>({...prev, mobile: ''}))
                         inputChange(e, setPhoneAuth)
                     }}
                     required
                 />
-                {phoneAuth?.mobile &&
+                {phoneAuth?.mobile && !inputs?.mobile &&
                     <button 
                         type="button"
                         onClick={onAuthSend}
@@ -87,8 +110,9 @@ export default function MobileAuthentication(inputs, setInputs) {
                         { !isPhoneNum ? '인증' : '재전송' }
                     </button>
                 }
+                { inputs?.mobile && <p>인증이 완료되었습니다.</p> }
             </div>
-            {isPhoneNum &&
+            {isPhoneNum && !inputs?.mobile &&
                 <div data-err-message="인증번호를 확인해주세요.">
                     <input 
                         type="text"
@@ -97,19 +121,25 @@ export default function MobileAuthentication(inputs, setInputs) {
                         data-formet='numb'
                         data-validation='mobileConfirm'
                         maxLength='6'
+                        ref={authRef}
                         onChange={(e)=>inputChange(e, setPhoneAuth)}
-                        // onBlur={(e)=>inputErrCheck(e)}
                         required
                     />
-                    {phoneAuth?.mobileAuth &&
-                        <button 
-                            type="button"
-                            onClick={onAuthCheck}
-                        >
-                            확인
-                        </button>
-                    }
-                    {formatTime(timeLeft)}
+                    <div>
+                        {timeLeft &&
+                            <time>
+                                {formatTime(timeLeft)}
+                            </time>
+                        }
+                        {phoneAuth?.mobileAuth &&
+                            <button 
+                                type="button"
+                                onClick={onAuthCheck}
+                            >
+                                확인
+                            </button>
+                        }
+                    </div>
                 </div>
             }
             {popup && <Popup popup={popup} setPopup={setPopup}/>}
